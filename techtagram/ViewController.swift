8f3f0c2af7d852841554a0b48a591441e3d08a37
daisukesone <sonedai0213@gmail.com>
2018-02-15 07:57:08 +0100

    カメラ使えない

--- techtagram/ViewController.swift
+++ techtagram/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var cameraImageView: UIImageView!
+    
+    //画像加工するための元となる画像
+    var originalImage: UIImage!
+    
+    //画像加工するフィルターの宣言
+    var filter: CIFilter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +27,75 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //撮影する時のメソッド
+    @IBAction func useCamera(){
+        //カメラが使えるかの確認
+         if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            //カメラを起動
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        } else{
+            //カメラが使えない場合はエラーがコンソールに出る
+            print("error")
+            
+        }
+        
+    }
+    
+    //カメラ、カメラロールを使ったときに選択した画像をアプリ内に表示させるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        cameraImageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        
+        originalImage = cameraImageView.image
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    //表示している画像にフィルター加工する時のメソッド
+    @IBAction func applyFilter(){
+        
+        let filterImage: CIImage = CIImage(image: originalImage)!
+        
+        //フィルターの設定
+        filter = CIFilter(name: "CIColerControls")!
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        
+        //彩度の調整
+        filter.setValue(1.0	, forKey: "inputBrightness")
+        
+        //コントラストの調整
+        filter.setValue(2.5, forKey: "inputContrast")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        cameraImageView.image = UIImage(cgImage: cgImage!)
+        
+        
+        
+    }
+    
+    //編集した画像を保存するためのメソッド
+    @IBAction func save(){
+        
+    }
+    
+    //カメラロールにある画像を読み込む時のメソッド
+    @IBAction func openAlbum(){
+        
+    }
 
+    //編集した画像をシェアする時のメソッド
+    @IBAction func share(){
+        
+    }
+    
+    
 
 }
 
